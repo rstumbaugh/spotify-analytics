0ac0d052fac517f6b9a1d93dcd9f1016289202e8
Ryan Stumbaugh <rstumbaugh1@gmail.com>
2018-08-18 18:01:10 -0400

    removed unnecessary env variable setting

--- scripts/init_env.sh
+++ scripts/init_env.sh
@@ -10,9 +10,5 @@ rm -rf $venv_dir
 python3 -m venv $venv_dir
 source $venv_dir/bin/activate
 
-
 echo [init_env] Installing dependencies from $app_dir/requirements.txt
 pip install -r $app_dir/requirements.txt
-
-echo [init_env] Setting environment variables
-source $app_dir/.env
